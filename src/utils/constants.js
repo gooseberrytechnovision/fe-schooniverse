f869198ba449ebebbaa8767f1d67e641cc0cf4ca
Lubin Raj <66677910+LubinRaj@users.noreply.github.com>
2025-05-19 00:21:00 +0530

    update url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const ROLES = {
   ALL: "all",
 };
 
-export const API_BASE_URL = "http://dev-env.eba-w6szmmmp.us-east-1.elasticbeanstalk.com";
+export const API_BASE_URL = "https://snmpg4arm2.us-east-1.awsapprunner.com";
   // "https://nestschoolbackend-production.up.railway.app";
 
 export const PAYMENT_MODE = {
